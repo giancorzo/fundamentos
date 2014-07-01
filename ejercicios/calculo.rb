ab43df4898afe7f0e53fcda6e8aa4be889e0eda3
= <=>
2014-07-01 02:11:50 -0500

    ejercicios

---
+++ ejercicios/calculo.rb
@@ -0,0 +1,25 @@
+#encoding: utf-8
+#
+# Cree un programa que permita calcular el Área Lateral, Área Total y Volumen 
+# de una Pirámide Hexagonal Regular, dados como datos el lado 
+# de la base (Lado) y el apotema de la pirámide (Apotema). 
+#
+# Use las siguientes fórmulas:
+#
+# altura = raiz(apotema^2 - 0.75*lado^2)
+#
+# area lateral = 3 * Lado * Apotema
+#
+# Area Total = Area Lateral + Lado^2 * (1.7321 * 6 / 4)
+#
+# Volumen = ( Lado^2 * 1.7321 * 6 / 4) * Altura / 3
+#
+# Donde el apotema es la altura de una de sus caras triangulares.
+#
+# Pasos:
+# 1. Crear función para calcular la altura
+# 2. Crear función para calcular el área
+# 3. Crear función para calcular el volumen
+# 4. Crear un procedimiento que usa las funciones e imprime los valores
+
+
---
+++ ejercicios/calculo02.rb
@@ -0,0 +1,14 @@
+#encoding: utf-8
+# Defina el análisis y diseño de un programa que reciba tres números. 
+# Estos números representan las longitudes de los lados de un triángulo. 
+#
+# Encuentre su área que se calcula mediante la siguiente fórmula:
+#
+# area= raiz(s*(s−a)*(s−b)*(s−c))
+#
+# Donde s es la mitad de la suma de los lados a, b y c. 
+# Utilice nombres apropiados para los datos de entrada y salida así como 
+# los utilizados en el proceso de cálculo.
+
+
+
