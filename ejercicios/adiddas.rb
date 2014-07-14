5da2c015798b810d71c243b58aecdf9cfa1ef862
= <=>
2014-07-14 02:55:59 -0500

    ejercicios

---
+++ ejercicios/adiddas.rb
@@ -0,0 +1,35 @@
+# La empresa Adidas, en vista del desarrollo del mundial de fútbol, ha desarrollado una
+# campaña llamada “adelgaza con el mundial”, para lo cual se ha desarrollado un sistema que
+# mide las calorías de acuerdo a las actividades que realiza la persona. En el sistema, una
+# persona ingresa las actividades realizadas en el día: Caminata, Bicicleta y Baile, además el
+# tiempo que utilizó en cada una. Esto le da un total de calorías consumidas. Las actividades y
+# tiempos son:
+#
+# Caminata (150 Calorías por cada 10 minutos)
+# Bicicleta (200 Calorías por cada 20 minutos)
+# Baile (250 Calorías por cada 30 minutos)
+#
+# Se solicita la elaboración del análisis y diseño de un programa del sistema explicado que
+# muestra como resultado el total de calorías consumidas por la persona
+
+def adiddas(actividad, tiempo)
+end
+
+def validate(expected, value)    
+  expected == value ? "." : "F"
+end
+
+def test_adiddas
+  print validate(1800.0,adiddas("caminata",120))
+  print validate(120.0,adiddas("bicicleta",12))
+  print validate(500.0,adiddas("baile",60))
+end
+
+def test
+  puts "Test de prueba del programa"
+  puts "---------------------------"
+  test_adiddas
+  puts " "
+end
+
+test
---
+++ ejercicios/calorias.rb
@@ -0,0 +1,38 @@
+# Una persona que se encuentra enferma pesa 70 kilos, desea saber cuántas calorías
+# consume su cuerpo en una cantidad de horas, sabiendo que las actividades permitidas son:
+# dormir y estar en reposo.
+#
+# Considere que estando en la actividad Dormir la persona consume 1.08 calorías por minuto y
+# en la actividad Reposo, consume 1.66 por minuto.
+#
+# Desarrolle el análisis y diseño del programa que permita ubicar la solución al problema
+# planteado.
+
+def calorias(actividad,horas)
+  case (actividad)
+   when "reposo"
+	return 1.08 * (horas * 60)
+   when "dormir"
+	return 1.66 * (horas * 60)
+  end
+end
+
+def validate(expected, value)
+  expected == value ? "." : "F"
+end
+
+def test_calorias
+  print validate(1195.2,calorias("dormir",12))
+  print validate(2592.0,calorias("reposo",40))
+  print validate(5976.0,calorias("dormir",60))
+end
+
+def test
+  puts "Test de prueba del programa"
+  puts "---------------------------"
+  test_calorias
+  puts " "
+end
+
+test
+
